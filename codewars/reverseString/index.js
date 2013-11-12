3bc8774f84fa5304f4c4a6bba1af87d261112f82
Andrew <yu.drew@gmail.com>
2013-11-12 12:00:41 -0800

    Reverse string exercise complete

--- codewars/reverseString/index.js
+++ codewars/reverseString/index.js
@@ -4,12 +4,15 @@ Complete the solution so that it reverses the string value passed into it.
 
 solution('world'); // returns 'dlrow'
 
+//Initial thoughts
+        //breakup string taking last letters off it
+        // append string to an array
+        // concatenate strings in the array 
 
 function solution(str) {
-  //breakup string taking last letters off it
-  // append string to an array
-  // concatenate strings in the array 
+  return str.split('').reverse().join('');
 }
 
-
 solution('world'); // returns 'dlrow'
+
+// This was also highest voted solution 
