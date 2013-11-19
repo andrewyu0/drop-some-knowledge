7f6cb814299f7bd28ad91066b76d705ca0db2ac0
Andrew <yu.drew@gmail.com>
2013-11-19 14:43:35 -0800

    Filter exercise complete

--- codewars/filter/index.js
+++ codewars/filter/index.js
@@ -1,7 +1,7 @@
 // Nov 14
 // Javascript filter - 3
 
-/******* Filter 1 exercise, referenced to by Filter 3 kata
+/***************************** Filter 1 exercise, referenced to by Filter 3 kata
 
 Log ins beginning with "_" are causing poblems. So, write a function that takes an array of pairs of login-names and e-mails, and outputs an array of all login-name, e-mails-pairs from the login-names that end with "_".
 
@@ -27,7 +27,16 @@ function searchNames(logins) {
 
 
 
-/*************Filter 3 exercise **************/
+// Another solution using regex 
+
+function searchNames( logins ){
+  return logins.filter(function(v) {
+    return /_$/.test(v[0]); // $ sign means end of string(/string$/), ^ means the beginning  
+  });
+}
+
+
+/***********************************Filter 3 exercise **************/
 
 /* User array is a flat array of user-email-pairs, like so:
 [ "foo", "foo@bar.com", "bar", "bar@foo.com", ".foo", "food@bar.com" ]
@@ -39,17 +48,37 @@ Only need to return the email address
 You have to use the filter-method of Javascript, which returns each element of the array for which the filter-method returns true. The second argument gives the index you're looking at and the third argument is the array itself: */
 
 function searchNames(logins) {
-  var array = return logins.filter(function(element){
-    return element.slice(-1) === '.' || element.slice(0) === '.'; 
+  return logins.filter(function(element,i,arr){
+    // arr[i] === element
+    // use filter optional params, (item, index, array), play with these 
+    // if statement 
+    // make sure index is greater than 0, first of all
+    if (i > 0 && (arr[i - 1].slice(-1) === '.' || arr[i - 1].slice(0,1) === '.')){
+      return true; 
+    } 
+    else {
+      return false;
+    }
   });
+}
+// Note, slice is tricky. 
 
-//This gives me an array of users that end or begin with ".". Now need to return an array with the emails that follow... 
+// With Regex
+function searchNames(logins) {
+  return logins.filter(function(element,i,arr){
+    if (i > 0 && (/\.$/.test(arr[i - 1]) || /^\./.test(arr[i - 1]))) { // special characters like period need to be 'escaped' using "\"" 
+      return true; 
+    } 
+    else {
+      return false;
+    }
+  });
+}
 
-// e.g.
-// logins = [".bar", "bar@bar.com", "hello", "hello@hello.com", ".this", "this@this.com" ]
-// I have  [".bar", ".this"]
-// I need ["bar@bar.com", "this@this.com"]
+// Highest rated answer
 
-
-
-
+function searchNames( logins ){
+  return logins.filter( function( value, index, a ){
+    return ( ( index % 2 ) == 1 ) && a[index - 1].match( /(^\.|\.$)/ );
+  } );
+}
