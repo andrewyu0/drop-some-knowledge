e9861241afc6f02169cff14860b56c7edab7a28e
Andrew <andrew.yu@cirruscpq.com>
2013-12-28 12:39:39 -0800

    insertionSort updated

--- codewars/algorithms/InsertionSort.js
+++ codewars/algorithms/InsertionSort.js
@@ -16,30 +16,32 @@ Time Complexity:
     - best:    O(n)
 Space Complexity:
     - O(n)
+
+When humans sort something manually, usually use this     
 */
 
-function insertion_sort(l) {
-    var copy = l;
-    var size = copy.length;
-    for (var i = 1; i < size; i++) {
-        for (var j = i; j > 0; j--) {
-            // iterate in reverse until element copy[j] >= copy[j-1]
-            if (copy[j] < copy[j-1]) {
-                // if elements are out of order,
-                // swap them using a temporary variable
-                var temp = copy[j-1];
-                copy[j-1] = copy[j];
-                copy[j] = temp;
-            } else {
-                break
-            }
-        }
+function insertionSort(array) {
+  var size = array.length;
+  for (var i = 1; i < size; i++) {
+    // iterate in reverse until element copy[j] >= copy[j-1]
+    // copy[j] should be >= copy[j-1]
+    for (var j = i; j > 0; j --) {
+      // if elements are out of order swap them using a temporary variable
+      // e.g. using [2,1] to illustrate:
+      if (array[j] < array[j-1]) {
+        var temp = array[j-1]; // temp is 2
+        array[j-1] = array[j]; // 2 is now 1
+        array[j] = temp; // 1 is now 2
+      } else {
+        // out of loop, i++ to next element
+        break
+      }
     }
-    return copy;
-}
+  }
+  return array;
+};
 
-// Testing
-var l = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
-console.log("Unsorted list: ", l);
-l = insertion_sort(l);
-console.log("Sorted list: ", l);
+var array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+console.log("Unsorted list: ", array); // "Unsorted list: [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]"
+array = insertionSort(array);
+console.log("Sorted list: ", array); // "Sorted list "
