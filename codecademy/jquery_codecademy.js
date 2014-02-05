83d398a8a9bedbd2aafae732316d1ef60690f2d3
Andrew <andrew.yu@cirruscpq.com>
2014-02-05 14:32:54 -0800

    more jQuery codecademy

--- codecademy/jquery_codecademy.js
+++ codecademy/jquery_codecademy.js
@@ -1,6 +1,7 @@
 // jQuery 
 
 // Language + Terms 
+
 // Example below:
 // .click() = "Event handler" on an element, they handle jQuery events
 // .fadeOut() = "jQuery Event", the actual event that occurs
@@ -145,6 +146,7 @@ $(document).ready(function(){
 ///////////////////////////////////////////
 ///////// Modifying HTML Elements /////////
 ///////////////////////////////////////////
+// dynamically update the content of your HTML page, including adding and removing elements. Gandle manipulating the DOM
 
 // Creating HTML Elements 
 
@@ -210,4 +212,123 @@ $(document).ready(function(){
         $(this).toggleClass('highlighted');
     })
 })
+
+// Changing individual elements
+
+//Example: Change the <div> in our HTML document so it has a height of 200 pixels, a width of 200 pixels, and a border-radius of 10 pixels. Let's make this happen immediately, without the need for a click. No need for event handler
+$(document).ready(function() {
+    $("div").height("200px");
+    $("div").width("200px");
+    $("div").css("border-radius","10px");
+});
+
+// Modifying Content (.html(), .val())
+
+// .html() can be used to set the contents of the first element match it finds. For instance
+// grabs first div found, and changes text to: 
+$('div').html("I love jQuery!");
+
+// .val() is used to get the value of form elements. For example
+// would get the value of the first checked checkbox that jQuery finds.
+$('input:checkbox:checked').val();
+
+
+// "Mastering Manipulations" Exercise
+
+// Example: 
+// Call the .click() event handler on your #button. As usual, give your .click() a function() {} as an input. Inside your function's {}s, create the variable toAdd and set it equal to the contents of the input field.
+
+// Go ahead and .append() a <div> with class="item" to the .list div of your HTML document, then MAKE SURE to click your button to add an item—the exercise will wait for you to do so!
+$(document).ready(function(){
+    $('#button').click(function(){
+        var toAdd = $('input[name=checkListItem]').val();
+        $('.list').append('<div class="item">' + toAdd + '</div>');
+    })
+})
+
+// Modifying Elements (13/14) Good example to see how everhything ties together, revisit
+
+// Example: Add element in input, then remove by clicking
+// .on() event handler 
+// .on() as a general handler that takes the event, its selector, and an action as inputs.
+// Complete your program by adding the .on() event handler as described above. It should go inside your $(document).ready(), but after and outside your $('#button').click().
+
+$(document).ready(function(){
+    // When you click the button, goes to toAdd
+    $('#button').click(function(){
+        $(document).on('click', '.item', function(){
+            $(this).remove();
+        });
+        var toAdd = $('input[name=checkListItem]').val();
+        $('.list').append('<div class="item">' + toAdd + '</div>');
+    })
+})
+
+
+/////////////////////////////////////
+////////// jQuery Events ////////////
+/////////////////////////////////////
+
+// General Structure
+$(document).ready(function() {
+    $('thingToTouch').event(function() {
+        $('thingToAffect').effect();
+    });
+});
+
+// ...to have effect occur right away, remove event
+$(document).ready(function(){
+    $('thingToAffect').effect();
+});
+
+// (3/12) Use the event handler .click() to make it fadeOut fast
+$(document).ready(function() {
+    $('div').click(function() {
+        $('div').fadeOut('fast');
+    });
+});
+
+// (4/12) "When you hover over the div, make it add the 'red' class to the div (making it red)"
+
+$(document).ready(function(){
+    $('div').hover(function(){
+        $(this).addClass('red');
+    });
+});
+
+// .dblclick() event 
+// "Double click a div and make it fadeOut fast"
+$(document).ready(function(){
+    $('div').dblclick(function(){
+        $(this).fadeOut('fast');
+    });
+});
+
+// .mouseenter() & .mouseleave()
+// On .mouseenter(), have your 'div' .fadeTo() an opacity of 1. (Use 'fast' as the first input and 1 as the second.)
+// On .mouseleave(), have your 'div' .fadeTo() an opacity of 0.25. (Again, use 'fast' as the first input.)
+$(document).ready(function(){
+    $('div').mouseenter(function(){
+        $(this).fadeTo('fast', '1');
+    });
+    $('div').mouseleave(function(){
+        $(this).fadeTo('fast', '0.25');
+    });
+});
+
+// .focus()
+// We say an element has focus when we click on it or tab over to it. If you've ever filled out a form on a web page and seen how each text box lights up when you tab to it or click on it, you've seen focus in action
+// The .focus() event handler only works on elements that can receive focus—the list of these elements is a bit vague, but HTML elements like <textarea>s and <input>s are the usual suspects.
+
+// Example:
+// Turn 'input' into a jQuery element and call the .focus() event handler on it.
+// Inside your event handler function, call .css() on your 'input' element and pass it two inputs: the CSS element to change ('outline-color') and the color to change it to ('#FF0000').
+
+$(document).ready(function(){
+    $('input').focus(function(){
+        $(this).css('outline-color', '#FF0000');
+    });
+});
+
+//.keydown() event & .animate() effect
 
