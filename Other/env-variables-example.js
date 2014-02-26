8243ea94102a9a0df17fe9719ba791118c008872
Andrew <andrew.yu@cirruscpq.com>
2014-02-26 10:40:27 -0800

    moved around files

--- Other/env-variables-example.js
+++ Other/env-variables-example.js
@@ -1,5 +1,6 @@
-// process = global provided by node
-// env = property on process, which is an object of all the env variables
+// process  = global provided by node
+// env      = property on process, which is an object of all the env variables
 // NODE_ENV = a specific env variable
+
 // what's passing through is an object, a collection of variables
 console.log(process.env.NODE_ENV);
